915293145b474c987de7039a294160d64fe4e542
Ashishadk9 <aashishadk82@gmail.com>
2025-06-06 22:42:09 +0545

    Add files via upload

---
+++ Add Set.py
@@ -0,0 +1,7 @@
+numbers ={21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
+print("Initial Set:", numbers)
+
+# using add() method
+numbers.add(31)
+
+print("Updated Set:", numbers)
---
+++ Empty Set.py
@@ -0,0 +1,8 @@
+# create an empty set
+empty_set= set()
+
+empty_dictionary ={ }
+# check data type of empty_set
+print("Data type of empty_set:", type(empty_set))
+# check data type of dictionary_set
+print("Data type of empty_dictionary:", type(empty_dictionary))
